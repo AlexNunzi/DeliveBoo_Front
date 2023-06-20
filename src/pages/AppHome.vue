ebf2a14af4bae8c6c2fc55a798ec0102d4965cbc
Andrea Torelli <gioran326@protonmail.com>
2023-06-20 09:47:21 +0200

    test chiamata api per restaurant

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from 'axios';
 import HeroHome from "../components/HeroHome.vue";
 
 export default {
@@ -7,8 +8,10 @@ export default {
       HeroHome
    },
    data() {
-      return{
-         types: ['Italiano', 'Cinese', 'Giapponese', 'Fast-Food', 'Pizzeria', 'Argentino', 'Kebab', 'Indiano', 'Thailandese'],
+      return {
+         types: [],
+         checked:[1,4],
+         restaurant: [],
          lista: false,
       }
    },
@@ -16,43 +19,77 @@ export default {
       check(index) {
          const label = document.getElementById(index);
          label.classList.toggle("checked")
-         //creare funzione che "toggla" ceccato o meno ogni volta
-         console.log("ceccato da label")
+         // if(){
 
+         // } else{
+         //    this.checked.push(index);
+         // }
+
+
+         //con if, controlla se il tipo/ristorante non è nell'array, lo aggiunge, se c'è lo toglie
+         //cicla array e mostra nella lista (dopo il click su "Lista"?!)
+
+         // this.getRestaurant();
+         console.log(index)
+      },
+      getTypes() {
+         this.loading = true;
+         axios.get(`http://127.0.0.1:8000/api/types/`)
+         .then(response => {
+            this.types = response.data.results;
+            console.log(this.types);
+         });
+         
+      },
+      getRestaurant() {
+         axios.get(`http://127.0.0.1:8000/api/restaurant/type`,
+         {
+            params: {
+               "type_id[]" : this.checked
+            }
+         })
+         .then(response => {
+            console.log(response.data);
+            this.restaurant = response.data.results;
+         });
       }
+   },
+   mounted() {
+      this.getTypes();
+      this.getRestaurant();
    }
 }
 </script>
 
 <template>
-<HeroHome>
-   
-</HeroHome>
-<form action="/" method="get" class="py-5">
-   <div class="row g-3 py-3 justify-content-around">
-      <div class="col-6 col-md-4 col-lg-3" v-for="(tipo, i) in types" >
-         <input :id="tipo" type="checkbox" class="d-none cecco" :value="i" :name="tipo">
-         <label :id="i" :for="tipo" class="carta bg-white text-center" @click="check(i)">
-            <img :src="`../../public/${tipo}.jpg`" :alt="'Immagine ristorante '+ tipo">
-            <div class="centrato">
-               <h6 class="p-2">{{ tipo }}</h6>
-            </div>
-         </label>
+   <HeroHome />
+   <form action="/" method="post" class="py-5">
+      <div class="row g-3 py-3 justify-content-around">
+         <div class="col-6 col-md-4 col-lg-3" v-for="(tipo, i) in types">
+            <input :id="tipo.id++" type="checkbox" class="d-none cecco" :value="tipo.id" name="type_id[]">
+            <label :id="i" :for="tipo.name" class="carta bg-white text-center" @click="check(tipo.id)">
+               <!-- <img :src="`../../public/${tipo.name}.jpg`" :alt="'Immagine ristorante ' + tipo.name"> -->
+               <div class="centrato">
+                  <h6 class="p-2">{{ tipo.name }}</h6>
+               </div>
+            </label>
+         </div>
       </div>
+      <button type="submit" class="btn btn-success">Cerca</button>
+   </form>
+   <div class="btn btn-primary" @click="lista = true">Lista</div>
+   <div class="lista" v-show="lista">
+      <h1>Qua ci sarà la lista dei ristoranti selezionati</h1>
    </div>
-   <button type="submit" class="btn btn-success">Cerca</button>
-</form>
-<div class="btn btn-primary"  @click="lista = true">Lista</div>
-<div class="lista" v-show="lista">
-   <h1>Qua ci sarà la lista dei ristoranti selezionati</h1>
-</div>
 </template>
 
 
 <style lang="scss" scoped>
 @use "../styles/general.scss";
 
-img, .carta, h6{
+img,
+.carta,
+h6 {
    transition: all 0.3s;
 }
 
@@ -62,46 +99,47 @@ img, .carta, h6{
    border: inset 5px gray;
    // filter: grayscale(85%);
 
-   &:hover{
+   &:hover {
       // filter: grayscale(0%);
 
-      img{
+      img {
          scale: 1.04;
       }
 
-      h6{
+      h6 {
          font-weight: bolder;
       }
    }
 
-   &:active{
+   &:active {
       scale: 0.98;
    }
-   
+
 }
+
 .checked {
    scale: 1.04;
    box-shadow: 0.5rem 0.5rem 1rem rgba(0, 0, 0, 0.4);
    border: outset 5px greenyellow;
    filter: grayscale(0%);
-   
-   h6{
+
+   h6 {
       font-weight: bolder;
    }
-   
-   &:hover{
-      img{
+
+   &:hover {
+      img {
          scale: 1;
       }
 
-      h6{
+      h6 {
          font-weight: bolder;
       }
    }
 }
 
 
-img{
+img {
    height: 8rem;
    width: 8rem;
    object-fit: fill;
@@ -110,7 +148,7 @@ img{
 
 @media all and (min-width: 768px) {
 
-   .carta{
+   .carta {
       flex-direction: row;
       border-radius: 0;
       display: flex;
@@ -119,8 +157,5 @@ img{
 }
 
 
-@media all and (min-width: 480px) {
-
-
-}
+@media all and (min-width: 480px) {}
 </style>
