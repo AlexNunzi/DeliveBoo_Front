9af037cfa18b011e4116ab2d653fb521f24a19d6
adelinashahaj <adelinarucaj@icloud.com>
2023-06-21 15:02:30 +0200

    ho cereato un altro compone

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,12 @@
 <script>
+import AppCart from './AppCart.vue'
 export default {
+  
    name: "AppHeader",
+  
+   components: {
+      AppCart
+   },
    data() {
       return {
 
@@ -38,19 +44,9 @@ export default {
                </ul>
                
             </div>
-            <i class="fa-brands fa-opencart position-relative" data-bs-toggle="dropdown">
-               <span class="fw-lighter position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">10</span>
-            </i>
-            <div class="d-flex align-items-center">
-               <div class="dropstart">
-                  <ul class="dropdown-menu">
-                     <li><a class="dropdown-item" href="#">Qui viene </a></li>
-                     <li><a class="dropdown-item" href="#">visualizzato </a></li>
-                     <li><a class="dropdown-item" href="#">il carrello</a></li>
-                     <li><a href="https://getbootstrap.com/docs/5.2/components/dropdowns/#forms">Oppure un form. cliccami</a></li>
-                  </ul>
-               </div>
-            </div>
+            <AppCart>
+               
+            </AppCart>
       </nav>
    </div>
 </template>
